7a7745cf7c35d8f4df3c9082d2d7108058cef4c1
Ty Hopp <hopp.ty.c@gmail.com>
2023-05-07 13:36:12 +0800

    Add key to options

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -9,7 +9,9 @@ function Select({ label: selectLabel, className, options = [], ...selectRest }: 
     <Label label={selectLabel} className={className}>
       <select className="bg-white dark:bg-slate-900" {...selectRest}>
         {options.map(({ label: optionLabel, ...optionRest }) => (
-          <option {...optionRest}>{optionLabel}</option>
+          <option key={optionLabel} {...optionRest}>
+            {optionLabel}
+          </option>
         ))}
       </select>
     </Label>
